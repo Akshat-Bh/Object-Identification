4686365222623fdbd19364424cd1d6633286bb2b
Akshat-Bh <118525576+Akshat-Bh@users.noreply.github.com>
2023-01-23 17:24:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,27 +9,41 @@ function preload()
 
 function setup() 
 {
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function start()
+{
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 
 function draw()
 {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
 
     if (dstatus != "")
     {
+        r= random(255);
+        g= random(255);
+        b= random(255);
+        objectDetector.detect(video, gotResult);
+
         for (i=0; i< objects.length; i++)
         {
             document.getElementById("status").innerHTML = "Status: Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of object(s) detected are: " + objects.length;
 
-            fill('#FF0000');
+            fill(r, g, b);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%", objects[i].x, object[i].y);
             noFill();
-            stroke("FF0000");
+            stroke(r, g, b);
+            //Needs fill function to work(it sets the border colour)
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
     }
@@ -39,7 +53,7 @@ function modelLoaded()
 {
     console.log("Model Loaded!");
     dstatus = true;
-    objectDetector.detect(img, gotResult);
+    objectDetector.detect(video, gotResult);
 }
 
 function gotResult(error, results)
